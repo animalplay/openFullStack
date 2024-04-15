879d56c484eed34e5b766224dc6e7d3e76db4f3d
animalplay <juandavidgonzalezurrego@gmail.com>
2024-04-14 22:24:57 -0500

    step one to deploy notes app

---
+++ parts/part-3/notes-backend/README.md
@@ -0,0 +1,4 @@
+Is very important to add CORS, to use your api in another application
+npm install cors
+
+To deploy the backend, I use render.com
--- parts/part-3/notes-backend/index.js
+++ parts/part-3/notes-backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 
 let notes = [
